dd89079d85876ecdf374c389465a3904ea0c01c8
Shashwat Singh <shashwat123student@gmail.com>
2024-01-12 18:42:32 +0530

    Implementiong the MVVM Model

--- app/src/main/java/com/example/androidprac/view/MainActivity.kt
+++ app/src/main/java/com/example/androidprac/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.androidprac
+package com.example.androidprac.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.androidprac.R
 import com.example.androidprac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
